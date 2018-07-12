8974c2dc8a5252c24f4b4bbd7acceefd024e3862
siarasmall <siarasmall19@gmail.com>
2018-07-12 15:44:14 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -17,7 +17,7 @@ end #def
 
 #3. Define a method `delete_artist`. This method should take delete the key-value pair from the hash based on the artist and returned the updated hash.
 #This method should take two arguments, the music library hash, and the artist. Remember, keys in hashes are symbols, so you'll need to expect the method to accept the artist as a symbol. 
-def delete_artist(library)(:artist)
+def delete_artist(library, :artist)
   library.delete(:artist)
   return library 
 end
@@ -25,7 +25,7 @@ end
 #4. This method should add a new artist and song (key value pair) to the hash and return the updated hash. 
 # This method should accept three arguments, the library hash, the artist, and the song.
 
-def add_song_and_artist(library)(:artist)(song)
+def add_song_and_artist(library, :artist, song)
   library[:artist]= "#{song}"
   return library
 end
